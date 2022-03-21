97d63e7fcc827603ff53a5c011cf6c5d3a7d5978
93TEI <43162525+93TEI@users.noreply.github.com>
2022-03-21 09:13:33 +0900

    Update stream-api2.java

--- stream-api2.java
+++ stream-api2.java
@@ -21,16 +21,18 @@ Predicate<T>
 
 일반 메소드 참조 ==> 클래스명::메소드
 */
-
-//filter
-Stream<String> stream = list.stream() .filter(name -> name.contains("a"));
-//map
-Stream<String> stream = names.stream() .map(s -> s.toUpperCase());
-//sorted
-Stream<String> stream = list.stream() .sorted(Comparator.reverseOrder());
-//distinct (equals와 hashcode를 오버라이드해야 기능이 동작함)
-Stream<String> stream = list.stream() .distinct();
-//peek ( stream에 영향을 주지 않고 확인(peek)해보는 용도)/ peek 파라미터 : 함수형 인터페이스 Consumer
-int sum = IntStream.of(1, 3, 5, 7, 9) .peek(System.out::println) .sum();
-//형변환
-Stream.of(1.0, 2.0, 3.0) .mapToInt(Double::intValue) .mapToObj(i -> "a" + i)
+/*중간연산*/
+   //filter
+   Stream<String> stream = list.stream() .filter(name -> name.contains("a"));
+   //map
+   Stream<String> stream = names.stream() .map(s -> s.toUpperCase());
+   //sorted
+   Stream<String> stream = list.stream() .sorted(Comparator.reverseOrder());
+   //distinct (equals와 hashcode를 오버라이드해야 기능이 동작함)
+   Stream<String> stream = list.stream() .distinct();
+   //peek ( stream에 영향을 주지 않고 확인(peek)해보는 용도)/ peek 파라미터 : 함수형 인터페이스 Consumer
+   int sum = IntStream.of(1, 3, 5, 7, 9) .peek(System.out::println) .sum();
+   //형변환
+   Stream.of(1.0, 2.0, 3.0) .mapToInt(Double::intValue) .mapToObj(i -> "a" + i)
+      
+/*최종연산*/
